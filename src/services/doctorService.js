bbeceac3f3bafbd487cbc698d5748e5f87cc4d9a
khanhlong0201 <96827499+khanhlong0201@users.noreply.github.com>
2022-04-04 02:53:26 +0700

    lan dau push len

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -97,7 +97,7 @@ let getDetailDoctorById = (inputId) => {
                             model: db.Markdown,
                             attributes: ['description','contentHTML','contentMarkdown']
                         },
-                        { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+                          { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
                         
 
                     ],
