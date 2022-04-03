8133fcd5900049becaba2b95535ad8ab9a7da2a6
khanhlong0201 <96827499+khanhlong0201@users.noreply.github.com>
2022-04-04 02:49:56 +0700

    first commit

--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,7 @@
 import express from "express";
 import homeController from "../controllers/homeController";
 import userController from "../controllers/userController";
+import doctorController from "../controllers/doctorController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
@@ -15,12 +16,22 @@ let initWebRoutes = (app) => {
     router.get('/delete-crud', homeController.deleteCRUD);
 
     router.post('/api/login', userController.handleLogin);
+    router.get('/api/get-all-users', userController.handleGetAllUsers);
+    router.post('/api/create-new-user', userController.handleCreateNewUser);
+    router.put('/api/edit-user', userController.handleEditUser);
+    router.delete('/api/delete-user', userController.handleDeleteUser);
+    router.get('/api/allcode', userController.getAllCode);
     
+    router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
+    router.get('/api/get-all-doctors', doctorController.getAllDoctors);
+    router.post('/api/save-infor-doctors', doctorController.postInforDoctor);
+    router.get('/api/get-detail-doctor-by-id', doctorController.getDetailDoctorById);
+
     router.get('/hoidanit',(req, res)=>{
         return res.send("Hello world with hoidanit");
     })
 
-    return app.use("/",router);
+    return app.use("/",router); 
 }
 
 module.exports = initWebRoutes;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,29 +1,27 @@
 import db from "../models/index"
 import bcrypt from 'bcryptjs';
+import { promise, reject } from "bcrypt/promises";
+import { response } from "express";
 let handleUserLogin = (email,password) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let isExist = await checkUserEmail(email);
             let userData = {};
-            console.log(password);
+            let isExist = await checkUserEmail(email);
+
             if (isExist) {
                 let user = await db.User.findOne({
-                    attributes: ['email', 'roleid','password'],
+                    attributes: ['email', 'roleid','password','firstName','lastName'],
                     where: { email: email },
                     raw: true,
-                    // exclude: [];
                 })
-                console.log(user.password)
                 if (user) {
                     let check = await bcrypt.compareSync(password, user.password);
                     console.log(check);
                     if (check) {
                         userData.errCode = 0;
                         userData.errMessage = `OK`;
-                        console.log(user);
-                        console.log('1');
                        delete user.password;
-                        userData.data = user;
+                        userData.user = user;
                     } else {
                         userData.errCode = 3;
                         userData.errMessage = `Wrong password`,
@@ -43,6 +41,8 @@ let handleUserLogin = (email,password) => {
         }
     })
 }   
+
+const salt = bcrypt.genSaltSync(10);
 
 // let compareUserPassword = () => {
 //     return new Promise((resolve, reject => {
@@ -73,6 +73,173 @@ let checkUserEmail = (email) => {
     })
 }
 
+let getAllUsers = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let users = '';
+            if (userId === 'ALL') {
+                users = await db.User.findAll({
+                    attributes: {
+                        exclude: ['password']
+                    }
+                })
+            }
+            if (userId && userId !== 'ALL') {
+                users = await db.User.findOne({
+                    where: { id: userId },
+                     attributes: {
+                        exclude: ['password']
+                    }
+                })
+            }
+
+            resolve(users);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let createNewUser = (data) => {
+    return new Promise( async (resolve, reject) => {
+        try {
+            let check = await checkUserEmail(data.email);
+            if (check === true) {
+                resolve({
+                    errCode: 1,
+                    errmessage: "Your email is already in used, Please try another emails"
+                });
+            } else {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phoneNumber: data.phoneNumber,
+                    gender: data.gender,
+                    roleId: data.roleId,
+                    positionId: data.positionId,
+                    image: data.avatar
+                })
+            }
+            
+            resolve({
+                errCode: 0,
+                message: "ok"
+            });
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let hashUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPassword = await bcrypt.hashSync(password, salt);
+            resolve(hashPassword);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let deleteUser = (userId) => {
+    return new Promise( async (resolve, reject) => {
+        let foundUser = await db.User.findOne({
+            where: { id: userId}
+        })
+        if (!foundUser) {
+            resolve({
+                errCode: 2,
+                errMessage: `the user is not exits`
+            })
+        } 
+            await db.User.destroy({
+                where: { id: userId }
+            });
+        resolve({
+            errCode: 0,
+            errMessage: `the user is deleted`
+        })
+    })
+
+}
+
+
+let updateUserData = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.id && !data.gender && !data.positionId && !data.roleId ) {
+                resolve({
+                    errCode: 2,
+                    errMessage: 'Missing required parameter'
+                })
+            }
+            let user = await db.User.findOne({
+                where: { id: data.id },
+                raw: false
+            });
+            if (user) {
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+                user.address = data.address;
+                user.gender = data.gender;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.phoneNumber = data.phoneNumber;
+                user.image = data.avatar;
+
+                await user.save();
+
+                resolve({
+                    errCode: 0,
+                    message: 'update the user successed'
+                });
+                    
+            } else {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'User not found'
+                });
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let getAllCodeService = (typeInput) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!typeInput) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing required Parameters"
+                });
+            } else {
+                let res = {};
+                let allcode = await db.Allcode.findAll({
+                    where: { type: typeInput}
+                })
+                res.errCode = 0;
+                res.data = allcode;
+                resolve(res);
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+
 module.exports = {
-    handleUserLogin: handleUserLogin
+    handleUserLogin: handleUserLogin,
+    getAllUsers: getAllUsers,
+    createNewUser: createNewUser,
+    deleteUser: deleteUser,
+    updateUserData: updateUserData,
+    getAllCodeService: getAllCodeService
 }
